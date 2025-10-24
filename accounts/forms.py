560628751a357af1c771b1dabe10328a8180b652
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-10-24 08:59:50 -0300

    register user funcionalities and the home page setup

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,21 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Enter your password'
+        }),
+        min_length=6
+    )
+    
     confirm_password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
-        label='Confirm Password'
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Confirm your password'
+        }),
+        min_length=6
+        
     )
     class Meta:
         model = User
@@ -22,3 +34,22 @@ class UserForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-input'}),
             'password': forms.PasswordInput(attrs={'class': 'form-input'}),
         }
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError({
+                "confirm_password": "Password does not match!. Please try again."
+            })
+            
+        return cleaned_data
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -2,42 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 
 def registerUser(request):
+
     if request.method == 'POST':
-        print(request.POST)
+
         form = UserForm(request.POST)
         if form.is_valid():
-            
-            # Create the user using the form
-            password = form.cleaned_data['password']
+
+            # O método save() do form já cuida de setar a senha
             user = form.save(commit=False)
-            user.set_password(password)
+            
             user.role = User.CUSTOMER
             user.save()
-            
-            # Create the user using create_user method
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            
-            user = User.objects.create_user( # type: ignore
-                first_name=first_name,
-                last_name=last_name,
-                username=username,
-                email=email,
-                password=password
-            )
-            user.role = User.CUSTOMER
-            user.save()
-            
-            return redirect('registerUser')
+            messages.success(request, 'Your account has been registered successfully.')
+            return redirect('registerUser')  # Ou redirecione para login
+        else:
+            print("Invalid form")
+            print(form.errors)
     else:
         form = UserForm()
-    context = {
-        'form': form
-    }
+    
+    context = {'form': form}
     return render(request, 'accounts/registerUser.html', context)
